05ce74a1a62af356fc51c1345df45f0788b1f4b6
John Sheehy <sheehy.john@gmail.com>
2021-04-07 05:18:58 +0100

    add physics

--- src/config.js
+++ src/config.js
@@ -2,12 +2,25 @@ import Phaser from 'phaser';
 
 export default {
   type: Phaser.AUTO,
-  parent: 'game',
+  parent: 'game',     //div in index.html
   backgroundColor: '#33A5E7',
   scale: {
-    width: 800,
-    height: 600,
+    width: 500,
+    height: 320,
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  render: {
+      pixelArt: true
+  },
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 750 },      // Demo - change 7 and add x
+      debug: false,
+      debugShowVelocity: true,
+      debugShowBody: true,
+      debugShowStaticBody: true,
+    }
   },
 };
